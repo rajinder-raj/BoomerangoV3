15545adfbb451c854e48d46f279820a9a080da17
networkingjohn <networkingwithjohnly@gmail.com>
2016-02-10 18:28:15 -0700

    added upload activity objects & code behind - john

--- app/src/main/java/boom/boomerangov3/Upload.java
+++ app/src/main/java/boom/boomerangov3/Upload.java
@@ -1,29 +1,74 @@
 package boom.boomerangov3;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class Upload extends AppCompatActivity {
+public class Upload extends AppCompatActivity implements View.OnClickListener{
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload, downloadImage;
+    Button bUploadImage, bDownloadImage;
+    EditText uploadImageName, downloadImageName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Get the images
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        downloadImage = (ImageView) findViewById(R.id.downloadImage);
+
+
+        // Get the button
+        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+        bDownloadImage = (Button) findViewById(R.id.bDownloadImage);
+
+
+        // Get the image name
+        uploadImageName = (EditText) findViewById(R.id.etUploadName);
+        downloadImageName = (EditText) findViewById(R.id.etDownloadName);
+
+        imageToUpload.setOnClickListener(this);
+        bUploadImage.setOnClickListener(this);
+        bDownloadImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.imageToUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+            case R.id.bUploadImage:
+
+                break;
+            case R.id.bDownloadImage:
+
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
     }
 
 }
