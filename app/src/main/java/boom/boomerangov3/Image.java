9c2a7a9f9d624d2b265426d281aee98aff1ce98b
networkingjohn <networkingwithjohnly@gmail.com>
2016-03-01 11:12:37 -0700

    Modify Image class to encode & decode Bitmaps

--- app/src/main/java/boom/boomerangov3/Image.java
+++ app/src/main/java/boom/boomerangov3/Image.java
@@ -1,6 +1,14 @@
 package boom.boomerangov3;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
+
+import java.io.ByteArrayOutputStream;
+
 
 /**
  * Created by jparsee(john) on 17/02/16.
@@ -10,8 +18,8 @@ public class Image {
     private String username = null;
     private ArrayList<LikeDislike> likesDislikes = new ArrayList<LikeDislike>();
 
-    public Image(String username, String image_serialized) {
-        this.image_serialized = image_serialized;
+    public Image(String username, Bitmap bitmap) {
+        image_serialized = serializeBitmap(bitmap);
         this.username = username;
     }
 
@@ -27,7 +35,33 @@ public class Image {
         return username;
     }
 
-    // Add a like/dislike of the image
+    /**
+     * Take a bitmap image, compress and serializes it
+     * @param bitmap - bitmap image
+     * @return - serialized bitmap, uncompressed
+     */
+    public String serializeBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); // TODO - comment here
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream); // reduce the size of the image to a jpeg
+        return Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT); // encodes it in a string
+    }
+
+    /**
+     * Get the image back as a bitmap
+     * TODO - test if encoding is correct
+     * @return - normal bitmap, compressed
+     */
+    public Bitmap getBitmap() {
+        byte[] bitmapByteArray = Base64.decode(image_serialized, Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapByteArray, 0, bitmapByteArray.length);
+        return bitmap;
+    }
+
+    /**
+     * Add a individual like/dislike by a user, if not done so already
+     * @param email - unique identifier of the user
+     * @param like - true for likes, false for dislikes
+     */
     public void addLikeDislike(String email, boolean like) {
         if (this.didLikeDisliked(email)) {
             LikeDislike newLikeDislike = new LikeDislike(email, like);
@@ -36,8 +70,11 @@ public class Image {
 
     }
 
-    // Find if the user's like/disliked the image
-    // todo fix this
+    /**
+     * Find if the user's like/disliked the image
+     * @param email - unique identifier of the user
+     * @return - true if the user liked/disliked, false if never set
+     */
     public boolean didLikeDisliked(String email) {
         for (LikeDislike eachLike: likesDislikes) {
             if (eachLike.getEmail().equals(email))
@@ -46,22 +83,26 @@ public class Image {
         return false;
     }
 
-    // Generates the total likes of the image
+    /**
+     * Get the total likes of the image
+     * @return - total likes
+     */
     public int getLikes() {
         int likes = 0;
         for (LikeDislike eachLike: likesDislikes) {
-            // Check eachLike likes the image
             if (eachLike.isLike())
                 likes++;
         }
         return likes;
     }
 
-    // Generates the total dislikes of the image
+    /**
+     * Generates the total dislikes of the image
+     * @return - total dislikes
+     */
     public int getDislikes() {
         int dislikes = 0;
         for (LikeDislike eachDislike: likesDislikes) {
-            // Check eachLike dislikes the image
             if (!eachDislike.isLike())
                 dislikes++;
         }
